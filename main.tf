9a5ec31895cf24e979a69f12cb63340e301e300c
Richard Annand <rj170590@gmail.com>
2020-12-10 22:49:54 +0000

    feat: moved proviers to 0.13 format

--- main.tf
+++ main.tf
@@ -1,5 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.18.0"
+    }
+  }
+}
+
 provider "aws" {
-  version = "3.18.0"
+  region = "eu-west-2"
+}
+
+provider "aws" {
   alias = "acm"
   region = "us-east-1"
 }
