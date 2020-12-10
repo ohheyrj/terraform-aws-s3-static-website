3c11c79ac11df32d1778757266cb7907e4c35334
Richard Annand <rj170590@gmail.com>
2020-12-10 20:36:38 +0000

    fix(acm.tf): removed hardcoded ref
    removed reference to domain in DNS records

--- acm.tf
+++ acm.tf
@@ -8,5 +8,5 @@ resource "aws_acm_certificate" "cert" {
 resource "aws_acm_certificate_validation" "cert_validation" {
   provider                = acm
   certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.ohheyrj_co_uk_cert_route53 : record.fqdn]
+  validation_record_fqdns = [for record in aws_route53_record.cert_records : record.fqdn]
 }
